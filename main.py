b6e23ba2d2a16b82f23dcf96e0e1c72b96238b7e
Mattias Björk <mattias.bjork.id1@gmail.com>
2023-05-21 01:54:19 +0200

    final

--- main.py
+++ main.py
@@ -1,2 +1,42 @@
-# list comprehension
+# # numbers = [1, 2, 3]
+# # new_list = ["snake" for n in numbers]
+# # print(new_list)
+# #
+# # snake = "snake"
+# # snakeList = [n for n in snake]
+# # print(snakeList)
+# #
+# # rangelist = [2*n for n in range(1,5)]
+# # print(rangelist)
+# #
+# # kaka = "kaka2"
+# # kakalist = [n for n in kaka if not n == "a"]
+# # print(kakalist)
+# #
+# names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+# big_names = [n.upper() for n in names if len(n) > 4]
+# print(big_names)
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
